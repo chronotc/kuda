42a349a70a9eae0d13d5bd1f5cccb1a3c8eb3846
Silla Tan <silla@bridgeapp.com.au>
2017-08-02 09:30:53 +1000

    rename for clarity

--- handlers/init.js
+++ handlers/init.js
@@ -2,26 +2,26 @@ const fileHandler = require('../lib/file-handler');
 const path = require('path');
 const { promptNewService, promptRemoteState } = require('../lib/prompts');
 
-const INIT_FILE_PATH = path.resolve(process.cwd(), 'pit.json');
+const PIT_JSON_FILE_PATH = path.resolve(process.cwd(), 'pit.json');
 
 module.exports = () => {
-  const exists = fileHandler.exists(INIT_FILE_PATH);
+  const exists = fileHandler.exists(PIT_JSON_FILE_PATH);
   if (exists) {
     return console.log('pit.json file exist. Repository is already initialized!');
   }
 
-  let initFile = {
+  let pitJson = {
     services: [],
     remoteState: ''
   };
 
   return promptNewService()
     .then(service => {
-      initFile.services.push({ name: service });
+      pitJson.services.push({ name: service });
       return fileHandler.verifyPackageJson(service);
     })
     .then(() => promptRemoteState())
-    .then(remoteState => initFile.remoteState = remoteState)
-    .then(() => fileHandler.writeJson(INIT_FILE_PATH, initFile));
+    .then(remoteState => pitJson.remoteState = remoteState)
+    .then(() => fileHandler.writeJson(PIT_JSON_FILE_PATH, pitJson));
 };
 
