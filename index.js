5cffa2a7b48ec0309e2f28557abb02915e4f42cc
Silla Tan <silla@bridgeapp.com.au>
2017-08-03 01:38:04 +1000

    add bin

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const SUPPORTED_COMMANDS = require('./commands');
 
 const { _ } = argv;
 
-// console.log(process.env);
-
 const capturedCommand = _.find(arg => !!SUPPORTED_COMMANDS[arg]);
 
 if (!capturedCommand) {
   throw new Error('Please use one of the following commands. [ init / add / deploy ]');
 }
 
-SUPPORTED_COMMANDS[capturedCommand].handler();
+module.exports = () => SUPPORTED_COMMANDS[capturedCommand].handler();
