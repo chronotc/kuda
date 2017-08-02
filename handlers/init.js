cfac1e1f06a72ba1950f7c0858e3619d81859474
Silla Tan <silla@bridgeapp.com.au>
2017-08-02 23:35:19 +1000

    add option to reinit

--- handlers/init.js
+++ handlers/init.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const { promptNewService, promptRemoteState } = require('../lib/prompts');
+const { promptNewService, promptRemoteState, promptReinitialize } = require('../lib/prompts');
 const fileHandler = require('../lib/file-handler');
 const KudaJsonHandler = require('../lib/kuda-json-handler');
 const PackageJsonHandler = require('../lib/package-json-handler');
@@ -14,10 +14,21 @@ const KUDA_JSON_FILE_PATH = path.resolve(process.cwd(), 'kuda.json');
 
 module.exports = () => {
   const exists = fileHandler.exists(KUDA_JSON_FILE_PATH);
-  // if (exists) {
-  //   return console.error('kuda.json file exist. Repository is already initialized!');
-  // }
+  if (exists) {
+    return promptReinitialize()
+      .then(reinitialize => {
+        if (!reinitialize) {
+          return null;
+        }
 
+        return initialize();
+      });
+  }
+
+  return initialize();
+};
+
+function initialize () {
   return fileHandler.writeJson(KUDA_JSON_FILE_PATH, {})
     .then(() => promptNewService())
     .then(service =>
@@ -30,4 +41,4 @@ module.exports = () => {
     .then(remoteStatePath => kudaJsonHandler.writeRemoteStatePath(remoteStatePath))
     .then(() => remoteStateHandler.createEmptyState())
     .catch(console.error);
-};
+}
